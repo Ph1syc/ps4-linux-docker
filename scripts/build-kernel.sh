9a1b4ff5df3c695d77deff7b823e47ae90f69b3e
FalsePhilosopher <joshgarland90@gmail.com>
2025-04-10 15:03:51 -0600

    update build.sh

--- scripts/build-kernel.sh
+++ scripts/build-kernel.sh
@@ -17,10 +17,16 @@ cd "$dir"
 make mrproper
 # Copy custom config
 cp "$config" .config
+mkdir -p lib/firmware/mrvl && cd lib/firmware/mrvl/ || exit
+sudo wget https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/mrvl/sd8897_uapsta.bin
+sudo wget https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/mrvl/sd8797_uapsta.bin
+cd ../../..
+# patch config file
+sed -i 's,CONFIG_EXTRA_FIRMWARE_DIR="/lib/firmware",CONFIG_EXTRA_FIRMWARE_DIR="lib/firmware",g' .config
 # Build the kernel
 make -j$(nproc)
-# Make bzImage
-make bzImage
 # Install the kernel modules
 make modules_install
+# Make bzImage
+make bzImage
 echo "Kernel build completed successfully!"
