616d2e1f70ef275b662a67beef7e3bce0dcbb638
FalsePhilosopher <joshgarland90@gmail.com>
2025-04-10 15:57:11 -0600

    update workflow

--- scripts/build-kernel.sh
+++ scripts/build-kernel.sh
@@ -6,17 +6,9 @@ set -e
 repo=https://github.com/crashniels/linux.git
 branch=ps4-linux-6.12.y
 dir=/kernel/linux
-config=/kernel/linux/config
-
-# Clone the kernel source if not already cloned
-if [ ! -d "$dir" ]; then
-    git clone --recurse-submodules --branch "$branch" "$repo" "$dir"
-fi
+git clone --recurse-submodules --branch "$branch" "$repo" "$dir"
 cd "$dir"
-# Clean previous builds
-make mrproper
-# Copy custom config
-cp "$config" .config
+cp config .config
 mkdir -p lib/firmware/mrvl && cd lib/firmware/mrvl/ || exit
 sudo wget https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/mrvl/sd8897_uapsta.bin
 sudo wget https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/mrvl/sd8797_uapsta.bin
@@ -27,6 +19,6 @@ sed -i 's,CONFIG_EXTRA_FIRMWARE_DIR="/lib/firmware",CONFIG_EXTRA_FIRMWARE_DIR="l
 make -j$(nproc)
 # Make bzImage
 make bzImage
-mkdir -p /kernel/linux/artifacts
+mkdir -p artifacts
 cp arch/x86/boot/bzImage /kernel/linux/artifacts
 echo "Kernel build completed successfully!"
